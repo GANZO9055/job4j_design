42873c55190a160ca13926f901fdc677d85b209a
GANZO9055 <kosta.maslihov@gmail.com>
2024-02-10 00:09:13 +0300

    FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,25 @@
+package ru.job4j.io;
+
+import java.io.FileInputStream;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (FileInputStream input = new FileInputStream("data/even.txt")) {
+            StringBuilder text = new StringBuilder();
+            int read;
+            while ((read = input.read()) != -1) {
+                text.append(((char) read));
+            }
+            String[] lines = text.toString().split(System.lineSeparator());
+            for (String line : lines) {
+                if (Integer.parseInt(line) % 2 == 0) {
+                    System.out.println("true");
+                    continue;
+                }
+                System.out.println("false");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
