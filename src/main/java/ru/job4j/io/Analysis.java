84bd1d24be89f5b418b044734e348a29ce827406
GANZO9055 <kosta.maslihov@gmail.com>
2024-02-13 13:22:15 +0300

    Анализ доступности сервера [#859]

---
+++ src/main/java/ru/job4j/io/Analysis.java
@@ -0,0 +1,36 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class Analysis {
+    public void unavailable(String source, String target) {
+        try (BufferedReader read = new BufferedReader(new FileReader(source));
+             PrintWriter writer = new PrintWriter(target)) {
+            String line;
+            String dateStart = null;
+            String dateFinish = null;
+            while ((line = read.readLine()) != null) {
+                String[] strings = line.split(" ", 2);
+                if (("400".equals(strings[0]) || "500".equals(strings[0]))
+                        && dateStart == null) {
+                    dateStart = strings[strings.length - 1];
+                }
+                if (("200".equals(strings[0]) || "300".equals(strings[0]))
+                        && dateStart != null) {
+                    dateFinish = strings[strings.length - 1];
+                }
+                if (dateStart != null && dateFinish != null) {
+                    writer.printf("%s;%s;\n", dateStart, dateFinish);
+                    dateStart = null;
+                    dateFinish = null;
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        Analysis analysis = new Analysis();
+        analysis.unavailable("data/server.log", "data/target.csv");
+    }
+}
