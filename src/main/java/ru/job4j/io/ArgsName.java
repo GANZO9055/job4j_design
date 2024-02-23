67db607b69fa08a4df4e13b0166f33906077b764
GANZO9055 <kosta.maslihov@gmail.com>
2024-02-23 22:38:19 +0300

    Именованные аргументы [#295518 #482610]

--- src/main/java/ru/job4j/io/ArgsName.java
+++ src/main/java/ru/job4j/io/ArgsName.java
@@ -9,7 +9,7 @@ public class ArgsName {
 
     public String get(String key) {
         if (values.get(key) == null) {
-            throw new IllegalArgumentException("This key: '" + key + "' is missing");
+            throw new IllegalArgumentException(String.format("This key: '%s' is missing", key));
         }
         return values.get(key);
     }
@@ -18,14 +18,14 @@ public class ArgsName {
         for (String arg : args) {
             var strings = arg.split("=", 2);
             if ("-".equals(strings[0])) {
-                throw new IllegalArgumentException("Error: This argument '"
-                        + arg
-                        + "' does not contain a key");
+                throw new IllegalArgumentException(
+                        String.format("Error: This argument '%s' does not contain a key", arg)
+                );
             }
             if (strings[1].isBlank()) {
-                throw new IllegalArgumentException("Error: This argument '"
-                        + arg
-                        + "' does not contain a value");
+                throw new IllegalArgumentException(
+                        String.format("Error: This argument '%s' does not contain a value", arg)
+                );
             }
             values.put(strings[0].substring(1), strings[1]);
         }
@@ -37,14 +37,18 @@ public class ArgsName {
         }
         for (String arg : args) {
             if (!arg.startsWith("-")) {
-                throw new IllegalArgumentException("Error: This argument '"
-                        + arg
-                        + "' does not start with a '-' character");
+                throw new IllegalArgumentException(
+                        String.format(
+                                "Error: This argument '%s' does not start with a '-' character",
+                                arg)
+                );
             }
             if (!arg.contains("=")) {
-                throw new IllegalArgumentException("Error: This argument '"
-                        + arg
-                        + "' does not contain an equal sign");
+                throw new IllegalArgumentException(
+                        String.format(
+                                "Error: This argument '%s' does not contain an equal sign",
+                                arg)
+                );
             }
         }
         ArgsName names = new ArgsName();
