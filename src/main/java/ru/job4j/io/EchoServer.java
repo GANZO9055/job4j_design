8b51739950bdcecea693a45b365734e005d67572
GANZO9055 <kosta.maslihov@gmail.com>
2024-03-04 20:49:20 +0300

    Бот [#7921 #484617]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -12,10 +12,19 @@ public class EchoServer {
                 try (OutputStream output = socket.getOutputStream();
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
+                    String string = input.readLine();
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                        if (input.readLine().contains("msg=Bye")) {
-                            server.close();
-                        }
+                    if (string.contains("Exit")) {
+                        server.close();
+                        continue;
+                    }
+                    if (string.contains("Hello")) {
+                        output.write("Hello".getBytes());
+                        continue;
+                    }
+                    if (string.contains("msg")) {
+                        output.write("What".getBytes());
+                    }
                     output.flush();
                 }
             }
