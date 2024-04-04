1855f74b2e50374a8c219c3ca72fd4605c0e4db9
GANZO9055 <kosta.maslihov@gmail.com>
2024-04-04 14:58:48 +0300

    Связи между таблицами [#373615 #489721]

--- src/main/java/ru/job4j/db/oneToMany.sql
+++ src/main/java/ru/job4j/db/oneToMany.sql
@@ -1,9 +1,9 @@
 create table cars(
     id serial primary key,
     model varchar(255)
+    person_id int references person(id)
 );
 create table person(
     id serial primary key,
-    name varchar(255),
-    car_id int references cars(id)
+    name varchar(255)
 );
