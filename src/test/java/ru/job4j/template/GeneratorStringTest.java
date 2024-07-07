78f4770c0bd8ad748d82a10e73748e3a8da59216
GANZO9055 <kosta.maslihov@gmail.com>
2024-07-07 15:51:50 +0300

    Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorStringTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.template;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class GeneratorStringTest {
+    @Test
+    public void whenAllKeysPresentThenSubstituteValues() {
+        Generator generator = new GeneratorString();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        args.put("subject", "you");
+        String result = generator.produce(template, args);
+        assertEquals("I am a Petr Arsentev, Who are you?", result);
+    }
+
+    @Test
+    public void whenMissingKeyThenThrowException() {
+        Generator generator = new GeneratorString();
+        String template = "I am a ${name}, Who are ${subject}?";
+        Map<String, String> args = new HashMap<>();
+        args.put("name", "Petr Arsentev");
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            generator.produce(template, args);
+        });
+        assertEquals("Missing key: subject", exception.getMessage());
+    }
+}
