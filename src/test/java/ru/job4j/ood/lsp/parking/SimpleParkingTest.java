10582fd536c0760d1e91a872c4dd053bcae3cf67
GANZO9055 <kosta.maslihov@gmail.com>
2024-07-19 22:09:50 +0300

    Парковка машин [#853 #505815]

--- src/test/java/ru/job4j/ood/lsp/parking/SimpleParkingTest.java
+++ src/test/java/ru/job4j/ood/lsp/parking/SimpleParkingTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 @Disabled
-public class SimpleParkingTest {
+class SimpleParkingTest {
 
     @Test
     void whenParkingCarAndTruck() {
