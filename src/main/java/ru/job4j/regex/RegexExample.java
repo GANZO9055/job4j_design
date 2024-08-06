055757560f419a9972a3b320215a8c69f9198bb9
GANZO9055 <kosta.maslihov@gmail.com>
2024-08-06 16:30:14 +0300

    Временное хранение. git stash. [#504832]

--- src/main/java/ru/job4j/regex/RegexExample.java
+++ src/main/java/ru/job4j/regex/RegexExample.java
@@ -7,12 +7,12 @@ public class RegexExample {
     public static void main(String[] args) {
         Pattern pattern = Pattern.compile("Я учусь на Job4j");
 
-        String textOne = "Я учусь на курсе Job4j";
+        String textOne = "Я учусь на Job4j";
         Matcher matcherOne = pattern.matcher(textOne);
         boolean isPresentOne = matcherOne.matches();
         System.out.println(isPresentOne);
 
-        String textTwo = "Я учусь на курсе Job4j";
+        String textTwo = "Я учусь на Job4j";
         Matcher matcherTwo = pattern.matcher(textTwo);
         boolean isPresentTwo = matcherTwo.matches();
         System.out.println(isPresentTwo);
